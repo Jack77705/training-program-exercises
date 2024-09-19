5a56410835a3b3dc46860bb29ab785cde827a54f
Jack Chamberlain <z5589825@ad.unsw.edu.au>
2024-09-19 15:43:02 +1000

    finished ex2, fixed ex3

--- exercises/js_dom_manipulation/ex2/ex2.js
+++ exercises/js_dom_manipulation/ex2/ex2.js
@@ -1,3 +1,19 @@
 console.log("Exercise 2")
 
 const main = document.querySelector("#display-box")
+
+// const textIds = ["text1", "text2", "text3"]
+
+// function displayText(textId) {
+//     for (const id of textIds) {
+//         if (id === textId) {
+//             document.querySelector(`#${id}`).style.display = "block"
+//         } else {
+//             document.querySelector(`#${id}`).style.display = "none"
+//         }
+//     }
+// }
+
+function displayText(textId) {
+    main.innerHTML = document.querySelector(`#${textId}`).innerHTML;
+}
