54a2d52153587549f41fa24c869044c217853518
Jack Chamberlain <z5589825@ad.unsw.edu.au>
2024-09-19 15:15:47 +1000

    ex1 finished

--- exercises/js_dom_manipulation/ex1/ex1.js
+++ exercises/js_dom_manipulation/ex1/ex1.js
@@ -1,4 +1,20 @@
-console.log("Yay! The javascript running")
+console.log("Yay! The javascript is running")
 
 // Cat picture source 
 // https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg
+
+document.getElementById("name").innerText = "Jack Chamberlain";
+// document.querySelector("#name").innerHTML
+
+const text = document.querySelector("#bigger");
+text.style.color = "green";
+text.style.fontSize = "60px";
+
+document.querySelector(".my-img").src = 
+    "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg";
+
+const squares = document.querySelectorAll(".square");
+
+for (const square of squares) {
+    square.classList.remove("hidden");
+}
